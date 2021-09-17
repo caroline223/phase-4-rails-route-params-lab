a95d5f07e69c8ff764921a4af27d8d8101f74064
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-09-17 00:06:27 -0400

    Complete assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student_id = Student.find(params[:id])
+    render json: student_id
+  end
+
 end
